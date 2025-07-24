4de5775b3798dba79c870ea129590694eeae8596
Eranga <erangac@gmail.com>
2025-07-25 00:18:41 +0530

    Corrected the url

--- src/constants.js
+++ src/constants.js
@@ -3,5 +3,5 @@ export const SOCKET_URL = API_BASE_URL;
 export const CHAT_UPLOAD_URL = `${API_BASE_URL}/chat/upload`; 
 export const LOGIN_URL = `${API_BASE_URL}/chat/users/login`;
 export const SIGNUP_URL = `${API_BASE_URL}/chat/users/signup`; 
-export const RESET_PASSWORD_URL = `${API_BASE_URL}/chat/Added dark modeusers/reset-password`; 
+export const RESET_PASSWORD_URL = `${API_BASE_URL}/chat/users/reset-password`; 
 export const PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/; 
